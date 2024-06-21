ce76d72d885fcd9d0261825b240da2e4b0e3d2bd
xujialiu <xujialiuphd@gmail.com>
2024-06-22 07:20:08 +0800

    chore: remove useless code

--- MainWindow.py
+++ MainWindow.py
@@ -10,16 +10,8 @@ from PySide6.QtWidgets import (
 from PySide6.QtCore import Qt
 
 
-class my_widget(QWidget):
-    def __init__(self):
-        super(my_widget, self).__init__()
-        text_edit = QTextEdit()
-        box = QHBoxLayout()
-        box.addWidget(text_edit)
-        self.setLayout(box)
-
-
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
         self.resize(1500, 800)
@@ -33,14 +25,12 @@ class MainWindow(QMainWindow):
         # Left dock widget
         self.left_dock = QDockWidget("Image Dock", self)
         self.left_dock.setObjectName("Left Dock")
-        
+
         # 设置能够dock在左右, 但是pyside6的代码似乎有问题
         # TopDockWidgetArea和BottomDockWidgetArea代表左右
         # LeftDockWidgetArea和RightDockWidgetArea代表上下
         self.left_dock.setAllowedAreas(Qt.TopDockWidgetArea | Qt.BottomDockWidgetArea)
-        self.left_dock.setFeatures(
-            QDockWidget.DockWidgetMovable
-        )
+        self.left_dock.setFeatures(QDockWidget.DockWidgetMovable)
         self.addDockWidget(Qt.TopDockWidgetArea, self.left_dock)
 
         # Right dock widget
