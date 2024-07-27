bf269553197d2095611af5c754b9424438fa17fd
xujialiu <xujialiuphd@gmail.com>
2024-07-27 18:07:16 +0800

    feat: add label_num_photo and rearrange layout

--- ImgDock.py
+++ ImgDock.py
@@ -36,57 +36,15 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_3.addWidget(self.widget_img)
 
-        self.verticalLayout = QVBoxLayout()
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.horizontalLayout.addItem(self.horizontalSpacer)
-
-        self.pushButton_previous = QPushButton(self.centralwidget)
-        self.pushButton_previous.setObjectName(u"pushButton_previous")
-        self.pushButton_previous.setEnabled(True)
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButton_previous.sizePolicy().hasHeightForWidth())
-        self.pushButton_previous.setSizePolicy(sizePolicy)
-        self.pushButton_previous.setMinimumSize(QSize(0, 0))
-
-        self.horizontalLayout.addWidget(self.pushButton_previous)
-
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.horizontalLayout.addItem(self.horizontalSpacer_2)
-
-        self.pushButton_next = QPushButton(self.centralwidget)
-        self.pushButton_next.setObjectName(u"pushButton_next")
-        sizePolicy.setHeightForWidth(self.pushButton_next.sizePolicy().hasHeightForWidth())
-        self.pushButton_next.setSizePolicy(sizePolicy)
-        self.pushButton_next.setMinimumSize(QSize(0, 0))
-
-        self.horizontalLayout.addWidget(self.pushButton_next)
-
-        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.horizontalLayout.addItem(self.horizontalSpacer_3)
-
-
-        self.verticalLayout.addLayout(self.horizontalLayout)
-
-
-        self.verticalLayout_3.addLayout(self.verticalLayout)
-
         self.horizontalLayout_4 = QHBoxLayout()
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
         self.line = QFrame(self.centralwidget)
         self.line.setObjectName(u"line")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.line.sizePolicy().hasHeightForWidth())
-        self.line.setSizePolicy(sizePolicy1)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.line.sizePolicy().hasHeightForWidth())
+        self.line.setSizePolicy(sizePolicy)
         self.line.setFrameShape(QFrame.HLine)
         self.line.setFrameShadow(QFrame.Sunken)
 
@@ -100,8 +58,8 @@ class Ui_MainWindow(object):
 
         self.line_2 = QFrame(self.centralwidget)
         self.line_2.setObjectName(u"line_2")
-        sizePolicy1.setHeightForWidth(self.line_2.sizePolicy().hasHeightForWidth())
-        self.line_2.setSizePolicy(sizePolicy1)
+        sizePolicy.setHeightForWidth(self.line_2.sizePolicy().hasHeightForWidth())
+        self.line_2.setSizePolicy(sizePolicy)
         self.line_2.setFrameShape(QFrame.HLine)
         self.line_2.setFrameShadow(QFrame.Sunken)
 
@@ -110,14 +68,14 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_3.addLayout(self.horizontalLayout_4)
 
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
         self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.horizontalLayout_2.addItem(self.horizontalSpacer_4)
-
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
 
@@ -138,19 +96,11 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_2.addWidget(self.spinBox_brightness)
 
-        self.horizontalSpacer_5 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.horizontalLayout_2.addItem(self.horizontalSpacer_5)
-
 
         self.verticalLayout_2.addLayout(self.horizontalLayout_2)
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.horizontalSpacer_6 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.horizontalLayout_3.addItem(self.horizontalSpacer_6)
-
         self.label_2 = QLabel(self.centralwidget)
         self.label_2.setObjectName(u"label_2")
 
@@ -172,33 +122,82 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_3.addWidget(self.spinBox_contrast)
 
-        self.horizontalSpacer_7 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.horizontalLayout_3.addItem(self.horizontalSpacer_7)
-
 
         self.verticalLayout_2.addLayout(self.horizontalLayout_3)
 
 
-        self.verticalLayout_3.addLayout(self.verticalLayout_2)
-
-        self.horizontalLayout_5 = QHBoxLayout()
-        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.horizontalSpacer_8 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.horizontalLayout_5.addItem(self.horizontalSpacer_8)
+        self.horizontalLayout_5.addLayout(self.verticalLayout_2)
 
         self.pushButton_img_reset = QPushButton(self.centralwidget)
         self.pushButton_img_reset.setObjectName(u"pushButton_img_reset")
 
         self.horizontalLayout_5.addWidget(self.pushButton_img_reset)
 
-        self.horizontalSpacer_9 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
-        self.horizontalLayout_5.addItem(self.horizontalSpacer_9)
+        self.horizontalLayout_6.addLayout(self.horizontalLayout_5)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+        self.pushButton_previous = QPushButton(self.centralwidget)
+        self.pushButton_previous.setObjectName(u"pushButton_previous")
+        self.pushButton_previous.setEnabled(True)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.pushButton_previous.sizePolicy().hasHeightForWidth())
+        self.pushButton_previous.setSizePolicy(sizePolicy1)
+        self.pushButton_previous.setMinimumSize(QSize(0, 0))
+
+        self.horizontalLayout.addWidget(self.pushButton_previous)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer_2)
+
+        self.pushButton_next = QPushButton(self.centralwidget)
+        self.pushButton_next.setObjectName(u"pushButton_next")
+        sizePolicy1.setHeightForWidth(self.pushButton_next.sizePolicy().hasHeightForWidth())
+        self.pushButton_next.setSizePolicy(sizePolicy1)
+        self.pushButton_next.setMinimumSize(QSize(0, 0))
+
+        self.horizontalLayout.addWidget(self.pushButton_next)
+
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer_3)
 
 
-        self.verticalLayout_3.addLayout(self.horizontalLayout_5)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_7 = QHBoxLayout()
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_7.addItem(self.horizontalSpacer_4)
+
+        self.label_num_photo = QLabel(self.centralwidget)
+        self.label_num_photo.setObjectName(u"label_num_photo")
+
+        self.horizontalLayout_7.addWidget(self.label_num_photo)
+
+        self.horizontalSpacer_5 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_7.addItem(self.horizontalSpacer_5)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_7)
+
+
+        self.horizontalLayout_6.addLayout(self.verticalLayout)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_6)
 
 
         self.verticalLayout_4.addLayout(self.verticalLayout_3)
@@ -219,11 +218,12 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.pushButton_previous.setText(QCoreApplication.translate("MainWindow", u"Previous", None))
-        self.pushButton_next.setText(QCoreApplication.translate("MainWindow", u"Next", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Image adjustment", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Brightness", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Contrast", None))
         self.pushButton_img_reset.setText(QCoreApplication.translate("MainWindow", u"Reset", None))
+        self.pushButton_previous.setText(QCoreApplication.translate("MainWindow", u"Previous", None))
+        self.pushButton_next.setText(QCoreApplication.translate("MainWindow", u"Next", None))
+        self.label_num_photo.setText(QCoreApplication.translate("MainWindow", u"NO. photo / Total photos:", None))
     # retranslateUi
 
